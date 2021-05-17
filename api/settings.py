867d448151e3299ad37dbfa071c4530921a25986
jsyme222 <jsyme222@gmail.com>
2021-05-17 14:53:54 -0600

    Added STATIC_ROOT to settings.py

--- api/settings.py
+++ api/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR, 'media')
 
